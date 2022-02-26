7cd5fb0eedd8351e9a336a5cfe538bb227457234
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-26 23:27:00 +0300

    created sget_image_by_id function

--- album/urls.py
+++ album/urls.py
@@ -6,5 +6,6 @@ from .views import *
 urlpatterns = [
   path('', photos, name="photos"),
   path('save-image/', save_image, name="save_image"),
+  path('delete-image/<integer pk>/', delete_image, name="delete_image"),
 
 ]
--- album/views.py
+++ album/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from .forms import ImageForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 # Create your views here.
@@ -20,8 +20,31 @@ def save_image(request):
             form.save()
             context['form'] = form
         return HttpResponseRedirect("/")
-
     else:
         form = ImageForm()
-
     return render(request, "album/photo_form.html",context)
+
+
+def delete_image(request, id):
+    Image.objects.filter(pk=id).delete()
+    return HttpResponseRedirect("/")
+
+
+def get_image_by_id(request, id):
+    image = Image.objects.get(pk=id)
+    return render(request, "album/photo_form.html", {"image" : image})
+
+
+
+def update_image(request, id):
+    return HttpResponse("Hello update age")
+
+
+def search_image(request, category):
+    images = Image.objects.filter(category=category)
+    return render(request, "album/photo_form.html", {"images": images})
+
+
+def filter_by_location(request, location):
+    images = Image.objects.filter(location=location)
+    return render(request, "album/photo_form.html", {"images" : images})

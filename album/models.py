b2599e63403b389c7d38a6203a35cde8b0ce2c6c
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-26 18:40:26 +0300

    models registration

--- album/models.py
+++ album/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 
 
-class Image(models.Model):
-    image = models.ImageField()
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    location = models.ForeignKey()
-    category = models.ForeignKey()
-
-
 class Location(models.Model):
     name = models.CharField(max_length=100)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+
+
 

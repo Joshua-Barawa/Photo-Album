26b92daac9f366b304be3b3a8170bc3412bbc4ff
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-26 18:57:06 +0300

    created string function

--- album/models.py
+++ album/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Image(models.Model):
@@ -15,6 +21,10 @@ class Image(models.Model):
     description = models.TextField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 
 

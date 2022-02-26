bed1543c422f045dfb462c93495bcf83ca6ff55d
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-26 18:34:08 +0300

    created three models Image, Location & Category

--- album/models.py
+++ album/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.ForeignKey()
+    category = models.ForeignKey()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+

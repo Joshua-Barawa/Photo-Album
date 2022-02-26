bf01673cfe675843d2c8a01367e8f015b73bb154
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-26 18:46:06 +0300

    made migrations

--- album/models.py
+++ album/models.py
@@ -13,8 +13,8 @@ class Image(models.Model):
     image = models.ImageField()
     name = models.CharField(max_length=100)
     description = models.TextField()
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 

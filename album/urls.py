378888a127c214def694500fd200f8a43a4f80a3
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-02-27 12:36:10 +0300

    created readmore page

--- album/urls.py
+++ album/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
   path('', photos, name="photos"),
   path('save-image/', save_image, name="save_image"),
   path('delete-image/<int:pk>/', delete_image, name="delete_image"),
+  path('get-image/<int:pk>/', delete_image, name="delete_image"),
 
 ]
